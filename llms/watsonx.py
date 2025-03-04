9d7aafe6a900a1667308c80332e67ec0027353fa
galatro <galatrogiovanni@gmail.com>
2025-03-04 19:40:40 +0100

    Added parameters

--- llms/watsonx.py
+++ llms/watsonx.py
@@ -61,7 +61,72 @@ class WatsonxComponent(LCModelComponent):
             advanced=False,
             required=True,
         ),
-        ]
+        IntInput(
+            name="max_tokens",
+            display_name="Max Tokens",
+            advanced=True,
+            info="The maximum number of tokens to generate.",
+            range_spec=RangeSpec(min=1, max=4096),
+        ),
+        IntInput(
+            name="min_tokens",
+            display_name="Min Tokens",
+            advanced=True,
+            info="The minimum number of tokens to generate.",
+            range_spec=RangeSpec(min=0, max=2048),
+        ),
+        DropdownInput(
+            name="decoding_method",
+            display_name="Decoding method",
+            advanced=True,
+            options=["greedy", "sample"],
+            value="greedy",
+        ),
+        FloatInput(
+            name="repetition_penalty",
+            display_name="Repetition Penalty",
+            advanced=True,
+            info="Penalty for repetition in generation.",
+            range_spec=RangeSpec(min=1.0, max=2.0),
+        ),
+        IntInput(
+            name="random_seed",
+            display_name="Random Seed",
+            advanced=True,
+            info="The random seed for the model.",
+        ),
+        SliderInput(
+            name="top_p",
+            display_name="Top P",
+            advanced=True,
+            info="The cumulative probability cutoff for token selection. Lower values mean sampling from a smaller, more top-weighted nucleus.",
+            range_spec=RangeSpec(min=0, max=1),
+            field_type="float",
+        ),
+        SliderInput(
+            name="top_k",
+            display_name="Top K",
+            advanced=True,
+            info="Sample from the k most likely next tokens at each step. Lower k focuses on higher probability tokens.",
+            range_spec=RangeSpec(min=1, max=100),
+            field_type="int",
+        ),
+        SliderInput(
+            name="temperature",
+            display_name="Temperature",
+            advanced=True,
+            info="Controls randomness, higher values increase diversity.",
+            range_spec=RangeSpec(min=0, max=2),
+            field_type="float",
+        ),
+        StrInput(
+            name="stop_sequence",
+            display_name="Stop Sequence",
+            advanced=True,
+            info="A sequence where the generation should stop.",
+            field_type="str",
+        ),
+    ]
 
     def build_model(self) -> LanguageModel:
         creds = Credentials(
@@ -70,11 +135,27 @@ class WatsonxComponent(LCModelComponent):
             url=self.url,
         )
 
+        generate_params = {
+            GenTextParamsMetaNames.MAX_NEW_TOKENS: self.max_tokens or 200,
+            GenTextParamsMetaNames.MIN_NEW_TOKENS: self.min_tokens or 0,
+            GenTextParamsMetaNames.DECODING_METHOD: self.decoding_method or "greedy",
+            GenTextParamsMetaNames.REPETITION_PENALTY: self.repetition_penalty or 1.0,
+            GenTextParamsMetaNames.RANDOM_SEED: self.random_seed or 33,
+            GenTextParamsMetaNames.STOP_SEQUENCES: [self.stop_sequence] if self.stop_sequence else [],
+        }
 
+        if generate_params[GenTextParamsMetaNames.DECODING_METHOD] == "sample":
+            generate_params.update(
+                {
+                    GenTextParamsMetaNames.TEMPERATURE: self.temperature or 0.5,
+                    GenTextParamsMetaNames.TOP_K: self.top_k or 1,
+                    GenTextParamsMetaNames.TOP_P: self.top_p or 0.2,
+                }
+            )
 
         model = ModelInference(
             model_id=self.model_name,
-            params={},
+            params=generate_params,
             credentials=creds,
             project_id=self.project_id,
         )
